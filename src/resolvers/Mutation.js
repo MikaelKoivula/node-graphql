9e117303ddc7f3097b9690afe05fedeb34a65ca3
Mikael Koivula <mikael.koivula@lianatech.com>
2021-04-16 16:52:21 +0300

    update mutations

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -16,10 +16,37 @@ const Mutation = {
 
 		return user;
 	},
+	updateUser(parent, args, { db }, info) {
+		const { id, data } = args;
+		const user = db.users.find((user) => user.id === id);
+
+		if (!user) {
+			throw new Error('User not found');
+		}
+
+		if (typeof data.email === 'string') {
+			const emailTaken = db.users.some((user) => user.email === data.email);
+			if (emailTaken) {
+				throw new Error('Email taken');
+			}
+
+			user.email = data.email;
+		}
+
+		if (typeof data.name === 'string') {
+			user.name = data.name;
+		}
+
+		if (typeof data.age !== 'undefined') {
+			user.age = data.age;
+		}
+
+		return user;
+	},
 	deleteUser(parent, args, { db }, info) {
 		const userIndex = db.users.findIndex((user) => user.id === args.id);
 		if (userIndex === -1) {
-			throw new Error('user not found');
+			throw new Error('User not found');
 		}
 		const deletedUsers = db.users.splice(userIndex, 1);
 
@@ -50,6 +77,27 @@ const Mutation = {
 
 		return post;
 	},
+	updatePost(parent, args, { db }, info) {
+		const { id, data } = args;
+		const post = db.posts.find((post) => post.id === id);
+
+		if (!post) {
+			throw new Error('Post not found');
+		}
+
+		if (typeof data.title === 'string') {
+			post.title = data.title;
+		}
+
+		if (typeof data.body === 'string') {
+			post.body = data.body;
+		}
+
+		if (typeof data.published === 'boolean') {
+			post.published = data.published;
+		}
+		return post;
+	},
 	deletePost(parent, args, { db }, info) {
 		const postIndex = db.posts.findIndex((post) => post.id === args.id);
 		if (postIndex === -1) {
@@ -78,6 +126,20 @@ const Mutation = {
 		};
 
 		db.comments.push(comment);
+
+		return comment;
+	},
+	updateComment(parent, args, { db }, info) {
+		const { id, data } = args;
+		const comment = db.comments.find((comment) => comment.id === id);
+
+		if (!comment) {
+			throw new Error('Comment not found');
+		}
+
+		if (typeof data.text === 'string') {
+			comment.text = data.text;
+		}
 
 		return comment;
 	},
